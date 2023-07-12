2da9e495389ccc7a4cb01e7064dafaaf6ccbf99a
MrCarbene <Muhammad_Fouda@proton.me>
2023-07-12 19:47:11 +0200

    add billboard page and form to create and update billboards.

--- dashboard/components/ui/api-alert.tsx
+++ dashboard/components/ui/api-alert.tsx
@@ -33,11 +33,13 @@ export const ApiAlert: React.FC<ApiAlertProps> = ({
 
   return (
     <Alert>
-      <Server className="h-4 w-4" />
-      <AlertTitle className="flex items-center gap-x-2">
-        {title}
-        <Badge variant={variantMap[variant]}>{textMap[variant]}</Badge>
-      </AlertTitle>
+      <div className="flex items-center">
+        <Server className="h-5 w-5" />
+        <AlertTitle className="flex items-center gap-x-2 pl-3">
+          {title}
+          <Badge variant={variantMap[variant]}>{textMap[variant]}</Badge>
+        </AlertTitle>
+      </div>
       <AlertDescription className="mt-4 flex items-center justify-between">
         <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
           {description}
